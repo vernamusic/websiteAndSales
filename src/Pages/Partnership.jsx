7f19644f7280c7059a8578b2f210d0b08794d48d
Amirhossein Ghaffari <amirvernaw@gmail.com>
2024-10-15 17:11:31 +0330

    Create financialBox , TechBox and DevBox.

--- src/Pages/Partnership.jsx
+++ src/Pages/Partnership.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Box } from '@mui/material';
 import MessageBox from '../components/partnership/MessageBox';
 import ReqBox from '../components/partnership/ReqBox';
+import FinancialBox from '../components/partnership/FinancialBox';
+import TechnologicalBox from '../components/partnership/TechnologicalBox';
+import DevBox from '../components/partnership/DevBox';
 
 const Partnership = () => {
     return (
@@ -45,6 +48,72 @@ const Partnership = () => {
                 }}
             >
                 <ReqBox />
+            </Box>
+            <Box
+                sx={{
+                    width: '100%',
+                    minHeight: {
+                        xs: '300px',
+                        sm: '450px',
+                        md: '540px',
+                        lg: '675px',
+                        xl: '900px',
+                    },
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <FinancialBox />
+            </Box>
+            <Box
+                sx={{
+                    width: '100%',
+                    minHeight: {
+                        xs: '300px',
+                        sm: '450px',
+                        md: '540px',
+                        lg: '675px',
+                        xl: '900px',
+                    },
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <TechnologicalBox />
+            </Box>
+            <Box
+                sx={{
+                    width: '100%',
+                    minHeight: {
+                        xs: '300px',
+                        sm: '450px',
+                        md: '540px',
+                        lg: '675px',
+                        xl: '900px',
+                    },
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <DevBox />
+            </Box>
+
+            <Box
+                sx={{
+                    width: '100%',
+                    minHeight: {
+                        xs: '300px',
+                        sm: '450px',
+                        md: '540px',
+                        lg: '675px',
+                        xl: '900px',
+                    },
+                    display: 'flex',
+                    justifyContent: 'center',
+                    bgcolor: 'black',
+                }}
+            >
+                <MessageBox />
             </Box>
         </Box>
     );
