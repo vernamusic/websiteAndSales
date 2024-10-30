d530209e94c0094e34090c16707faeda1623d87c
Amirhossein Ghaffari <amirvernaw@gmail.com>
2024-10-30 16:32:44 +0330

    Edit

--- src/components/home/RegBox.jsx
+++ src/components/home/RegBox.jsx
@@ -7,22 +7,24 @@ const theme = createTheme({
     typography: {
         h6: {
             fontFamily: 'sen',
-            fontSize: { xs: '11px', sm: '11px', md: '13px', lg: '16px', xl: '21px' },
-            color: "#F1F1F1",
-            letterSpacing: '0.4px',
+            fontSize: '1.04vw',
             lineHeight: 'normal',
+            letterSpacing: '0.4px',
+            color: "#F1F1F1",
+            textTransform: 'none',
         },
         h3: {
-            fontFamily: 'Lato',
+            fontFamily: "Lato",
             fontWeight: 700,
-            fontSize: { xs: '18px', sm: '22px', md: '24px', lg: '28px', xl: '35px' },
-            color: "#FFFFFF",
-            letterSpacing: '0.4px',
+            fontSize: '1.59vw',
+            color: "#F1F1F1",
+            textTransform: 'none',
         },
         button: {
             fontFamily: 'Lato',
-            fontSize: { xs: '10px', sm: '10px', md: '14px', lg: '16px', xl: '18px' },
-            color: "#FFFFFF",
+            fontSize: '0.94vw',
+            textTransform: 'none',
+            color: "#F1F1F1",
         },
         form: {
             fontFamily: 'Inter',
@@ -90,36 +92,54 @@ const RegBox = () => {
 
                     <Box
                         sx={{
-                            display: { xs: 'none', sm: 'flex' },
+                            display: { xs: 'none', sm: 'none', md: 'flex' },
                             alignItems: 'center',
                             flexDirection: 'row',
                             justifyContent: 'space-between',
-                            width: { xs: '300px', sm: '300px', md: '450px', lg: '480px', xl: '550px' },
-                            height: { xs: '25px', sm: '30px', md: '38px', lg: '45px', xl: '50px' },
+                            width: '28.65vw',
+                            height: '2.6vw',
                             gap: '2%',
                         }}
                     >
                         <FormControl variant="outlined" fullWidth sx={{ height: '100%' }}>
-                            <InputLabel htmlFor="email-input">Email address</InputLabel>
+                            <InputLabel
+                                htmlFor="email-input"
+                                sx={{
+                                    top: '50%',
+                                    transform: 'translateY(-50%)',
+                                    position: 'absolute',
+                                    left: '16px',
+                                    color: '#000',
+                                }}
+                                shrink={email.length > 0}
+                            >
+                                Email address
+                            </InputLabel>
                             <OutlinedInput
                                 id="email-input"
                                 autoComplete="off"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 sx={{
+                                    width: '18.7vw',
+                                    height: '2.7vw',
                                     backgroundColor: '#FFFFFF',
                                     borderRadius: '6px',
-                                    height: '100%',
                                     ...theme.typography.form,
+                                    paddingTop: '16px',
+                                    paddingBottom: '16px',
+                                    paddingLeft: '16px',
                                 }}
                             />
                         </FormControl>
+
 
                         <Button
                             variant="contained"
                             onClick={handleOpenDialog}
                             sx={{
                                 ...theme.typography.button,
+                                mt:0.5,
                                 borderRadius: '6px',
                                 backgroundColor: '#B50304',
                                 textTransform: 'none',
