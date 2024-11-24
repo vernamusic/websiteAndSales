3060cbf22a57e15f97ed74650b29ab97cc4ce366
Amirhossein Ghaffari <amirvernaw@gmail.com>
2024-11-25 01:59:26 +0330

    "Updated theme typography and layout styles in DownloadBox component"

--- src/components/Watch/DownloadBox.jsx
+++ src/components/Watch/DownloadBox.jsx
@@ -9,25 +9,25 @@ const theme = createTheme({
     typography: {
         h6: {
             fontFamily: 'Lato',
-            fontSize: '16px', 
+            fontSize: { xs: '10px', sm: '12.64px', md: '14.22px', lg: '18px' },
             fontWeight: 400,
             color: "#EEEEEE",
             letterSpacing: '0.4px',
-            lineHeight: '24px',
+            lineHeight: {xs:'14px',md:'18px',lg:'24px'},
         },
         h3: {
             fontFamily: 'Lato',
             fontWeight: 600,
-            fontSize: '24px', 
+            fontSize: { xs: '14px', sm: '18px', md: '26px', lg: '32px' },
             color: "#FFFFFF",
             letterSpacing: '0.4px',
             lineHeight: '24px'
         },
         button: {
             fontFamily: 'Lato',
-            fontSize: '14px',
+            fontSize: {xs:'8px',md:'10px',lg:'14px'},
             fontWeight: 600,
-            lineHeight: '14px', 
+            lineHeight: {xs:'8px',md:'10px',lg:'14px'}, 
             color: "#FCFCFC",
         },
     },
@@ -51,9 +51,9 @@ const Home = () => {
                 sx={{
                     position: 'relative',
                     maxHeight: '746px',
-                    minHeight: { xs: '500px', sm: '600px', md: '700px', lg: '746px' },
+                    minHeight: { xs: '188px', sm: '300px', md: '600px', lg: '750px' },
                     width: '100%',
-                    background: `linear-gradient(254deg, rgba(31, 31, 31, 0.30) 21.97%, rgba(31, 31, 31, 0.62) 40.54%, rgba(31, 31, 31, 0.81) 72.87%, rgba(31, 31, 31, 0.90) 100%), url(${background}) lightgray -100.091px 35.184px / 113.902% no-repeat`,
+                    background: `linear-gradient(253.12deg, rgba(31, 31, 31, 0.2) 26.9%, rgba(31, 31, 31, 0.42) 33.48%, rgba(31, 31, 31, 0.56) 40.79%, rgba(31, 31, 31, 0.707727) 51.75%, rgba(31, 31, 31, 0.9) 91.23%), url(${background})`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     backgroundRepeat: 'no-repeat',
@@ -63,13 +63,14 @@ const Home = () => {
                     sx={{
                         position: 'absolute',
                         zIndex: 1,
-                        ml: '11%',
-                        mt: '16%',
+                        ml: { xs: 5, sm: 10, md: 19, lg: 24, xl: 28 },
+                        mt: { xs: '44px', sm: '100px', md: '230px', lg: '318px' },
                         maxWidth:'40%',
                     }}
                 >
                     <Typography
                         sx={{
+                            width:'100%',
                             mb: '0.4vw',
                             ml: '-0.2vw',
                             lineHeight: '1.5',
@@ -81,21 +82,31 @@ const Home = () => {
                     <Typography
                         variant="h6"
                         sx={{
-                            ...theme.typography.h6, mb: '2em', mt: '1em'
+                            ...theme.typography.h6,
+                            display:{xs:'none',sm:'none',md:'block'},
                         }}
                     >
                     The Vitruvian Shield smart watch provides a versatile and comprehensive solution for vital signs monitoring, enabling researchers and healthcare professionals to gather and analyze vital sign data efficiently.
                     </Typography>
-                    <Box sx={{ display: 'flex', gap: '0.78vw', marginTop: '1.56vw',}}>
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            ...theme.typography.h6,
+                            display:{xs:'block',sm:'block',md:'none'},
+                            width:{xs:'250px',sm:'325px'},
+                        }}
+                    >
+                    The Vitruvian Shield smart watch provides a versatile and comprehensive solution for vital signs monitoring
+                    </Typography>
+                    <Box sx={{ display: 'flex', gap: {xs:'1.5vw',sm:'1.5vw',md:'0.78vw'}, marginTop: '1.56vw',}}>
                         <Button
                             variant="outlined"
                             sx={{
                                 padding:0,
-                                minWidth:0,
                                 display: 'flex',
                                 borderRadius: '4px',
-                                width: '138px',
-                                height: '42px',
+                                width: {xs:'55px',md:'90px',lg:'138px'},
+                                height: {xs:'20px',md:'28px',lg:'42px'},
                                 ...theme.typography.button,
                                 borderColor: 'white',
                                 color: 'white',
@@ -107,18 +118,17 @@ const Home = () => {
                         >
                             Contact us
                         </Button>
+
                         <Button
                             variant="contained"
                             onClick={handleOpenDialog}
                             sx={{
-                                padding:0,
-                                minWidth:0,
+                                ...theme.typography.button,
+                                padding: 0,
                                 display: 'flex',
                                 borderRadius: '4px',
-                                width: '138px',
-                                height: '42px',
-                                ...theme.typography.button,
-
+                                width: {xs:'55px',md:'90px',lg:'138px'},
+                                height: {xs:'20px',md:'28px',lg:'42px'},
                                 backgroundColor: '#B50304',
                                 textTransform: 'none',
                                 '&:hover': {
