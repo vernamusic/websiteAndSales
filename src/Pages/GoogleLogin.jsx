68a1296041fe7b3e3e425b7b82a8b35a52c98d86
unknown <mramir886@gmail.com>
2024-11-26 16:27:28 +0330

    fixing google login logic

--- src/Pages/GoogleLogin.jsx
+++ src/Pages/GoogleLogin.jsx
@@ -18,31 +18,28 @@ const GoogleLogin = () => {
     }, [searchParams]);
 
     const handleGoogleCallback = async (code) => {
-        try {
-            const response = await fetch("http://127.0.0.1:8000/api/v1/auth/google/callback/", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                credentials: "include",
-                body: JSON.stringify({ code }),
-            });
+        console.log(code)
+        const response = await fetch("http://127.0.0.1:8000/api/v1/auth/google/callback/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify({ code }),
+        });
+        console.log(response.code)
 
-            if (!response.ok) {
-                throw new Error("Failed to exchange code for tokens");
-            }
-
-            const data = await response.json();
-            localStorage.setItem("access_token", data.access_token);
-            localStorage.setItem("refresh_token", data.refresh_token);
-
-            window.location.href = "/dashboard";
-        } catch (error) {
-            console.error("Error during Google login:", error);
-            alert("Login failed");
-        } finally {
-            setLoading(false);
+        if (!response.ok) {
+            throw new Error("Failed to exchange code for tokens");
         }
+
+        const data = await response.json();
+        localStorage.setItem("access_token", data.access_token);
+        localStorage.setItem("refresh_token", data.refresh_token);
+        console.log("access:",data.access_token)
+        console.log("refresh:",data.refresh_token)
+
+        window.location.href = "/";
     };
 
     return <div>{loading ? "Processing Google Login..." : "Failed to process login. Please try again."}</div>;
